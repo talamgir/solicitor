c0df2e0fa032cd28721d6a9180826679ddda44d6
Taimur Alamgir <talamgir@law.gwu.edu>
2015-12-06 18:16:32 -0500

    controllers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,32 @@
 class ProfilesController < ApplicationController
 	 before_action :show, :authenticate_user!
 
+	 def new
+	 	@profile = Profile.new(profile_params)
+	 	@profile.user_id = params[:user_id]
+
+	 	@profile.save
+
+	 	
+	 end
+
+	 def create
+	 end
+
+	 def edit
+	 end
+
+	 def update
+	 end
+
 	 def show
 	 end
+
+	 def destroy
+	 end
+
+	 private
+
+	 def profile_params
+
 end
